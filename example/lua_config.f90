4f21fb8630de6d0e8d589fdb8e8da289cba18fed
Paweł Biernat <pawel.biernat@gmail.com>
2012-02-22 11:14:41 +0100

    minor changes to flu example

--- example/lua_config.f90
+++ example/lua_config.f90
@@ -20,38 +20,28 @@ program lua_config_test
   id = l%array_to_key(key)
   if( l%get(key, x1) == 0 ) then
      print *, trim(id)//" = ", x1
-  else
-     print *, "unable to read real ", trim(id)
   end if
 
   key(2) = "name"
   id = l%array_to_key(key)
   if( l%get(key, str) == 0 ) then
      print *, trim(id)//" = ", trim(str)
-  else
-     print *, "unable to read string ", trim(id)
   end if
 
   key(2) = "x"
   id = l%array_to_key(key)
   if( l%get_len(key, n) == 0 ) then
      allocate(x(n))
-     if( l%get(key, x) /= 0 ) then
-        print *, "unable to read array ", trim(id)
+     if( l%get(key, x) == 0 ) then
      end if
-  else
-     print *, "unable to read length of an array ", trim(id)
   end if
 
   key(2) = "f"
   id = l%array_to_key(key)
   if( l%get_len(key, n) == 0 ) then
      allocate(f(n))
-     if( l%get(key, f) /= 0 ) then
-        print *, "unable to read array ", trim(id)
+     if( l%get(key, f) == 0 ) then
      end if
-  else
-     print *, "unable to read length of an array ", trim(id)
   end if
 
   do i = 1, n
