361ff591129274e9731cf5a49c2027c259ebf907
Paweł Biernat <pawel.biernat@gmail.com>
2012-05-12 20:32:46 +0200

    Rewritten mesh and added a sfd3pt mesh

--- src/mesh/mesh.f90
+++ src/mesh/mesh.f90
@@ -7,230 +7,54 @@
 !! discrete representation of a set of functions defined on grid
 !! points.
 !!
-!! Programmer extending this class should provide approximations to
-!! derivatives of the functions with respect to mesh, and possibly an
-!! algorighm to calculate an integral of a givan function, see
-!! \ref mesh\%integrate.
-!!
 module class_mesh
 
+  use class_platonic
   use constants_module
   use logger_module
 
   private
 
-  !> @todo write an additional interface to the calculate_derivatives
-  !! etc. which does not use internal tables and takes input/output
-  !! data as arguments
-
-  !> skeleton mesh class to be extended by user-defined meshes
-  type, public, extends(named) :: mesh
-     !> number of mesh points, determines the size of x(nx)
-     integer                   :: nx = 0
-     !> rk holds a maximal rank of derivative that can be calculated
-     !! using this mesh
-     integer                   :: rk = 0
-     ! !> mesh should range from x0 to x1
-     ! real                      :: x0 = 0.0, x1 = 0.0
-     !> \f$x_i\f$
-     real, pointer             :: x(:) => null()
-     ! !> \f$\Delta x_i\f$
-     real, pointer             :: dx(:) => null()
-
+  type, public, abstract, extends(platonic) :: mesh
+     !> max_derivative holds a maximal rank of derivative that can be
+     !! calculated using this mesh
+     integer                   :: max_derivative = 0
+     integer                   :: ghost_points = 0
    contains
-     ! obligatory
-     procedure                  :: diff_point
-     procedure                  :: diff_global
-
-     ! optional
-     procedure                  :: init
-     procedure                  :: free
-     procedure                  :: generate_points
-     procedure                  :: integrate
-     procedure                  :: calculate_spacings
-
+     procedure(diff_global_interface), deferred :: diff_global
+     ! procedure(diff_point_interface ), deferred :: diff_point
   end type mesh
 
-contains
+  interface
+     !> Should calculate
+     !! \f$D_k f(x_i)\f$ for \f$k\f$ given as argument and save it to
+     !! df
+     !!
+     !! @param m
+     !! @param f function do differentiate
+     !! @param df derivative is going to be saved here
+     !! @param k rank of derivative to calculate
+     !!
+     subroutine diff_global_interface( m, f, x, df, k )
+       import mesh
+       class(mesh), target, intent(inout) :: m
+       integer, intent(in) :: k
+       real, intent(in)  :: f(:), x(:)
+       real, intent(out) :: df(:)
+     end subroutine diff_global_interface
 
-  !> does allocate(mesh%x(mesh%nx)), provided that x(:) has not been
-  !! already pointed to a location. In such case it sets nx based on
-  !! x(:). It also _assumes_ that dx is _not_ allocated, and tries to
-  !! allocate it to be of the same size as x(:).
-  !!
-  !! @param m
-  !!
-  !! @return
-  !!
-  subroutine init(m)
-    class(mesh), intent(inout) :: m
+     !> Same as diff_global, but calculates only the derivative at point i
+     !!
+     !! @return value of the k-th derivative at point x_i
+     !!
+     function diff_point_interface( m, f, x, k, i ) result(d)
+       import mesh
+       class(mesh), target, intent(inout) :: m
+       integer, intent(in) :: i,k
+       real, intent(in) :: f(:), x(:)
+       real :: d
+     end function diff_point_interface
 
-    if( .not. associated(m%x) .and. m % nx > 0 ) then
-       allocate( m % x ( m % nx ) )
-       allocate( m % dx( m % nx ) )
-       call m%log(FPDE_LOG_DEBUG, "init: generating from nx")
-    else
-       call m%log(FPDE_LOG_ERROR,&
-            "init: neither, x(:) nor nx were set correctly")
-    end if
-
-    if( m % rk < 0 ) then
-       call m%log(FPDE_LOG_ERROR,&
-            "mesh: init: rk < 0")
-    end if
-
-  end subroutine init
-
-  !> Overloaded should generate contents of x(:), using an algorithm
-  !! dependent on a perticular mesh implementation, e.g. uniformly or
-  !! gauss-lobatto-like. As a bonus, it updates nx and dx(:) to be
-  !! compatible with x(:). This function should be called _once_ per
-  !! mesh. The points x(1) and x(nx) determine boundaries of the grid.
-  !!
-  !! @param m
-  !!
-  !! @return
-  !!
-  subroutine generate_points(m)
-    class(mesh), intent(inout) :: m
-
-    ! if m % x is associated, then update nx to size(x) and reallocate
-    ! the table for dx
-    if( associated( m % x ) .and. size(m%x) /= m%nx ) then
-       ! update value of nx, in case x(:) has changed recentely
-       m % nx = size( m%x )
-       ! reallocate dx(:) to match the size of x(:)
-       deallocate(m%dx)
-       allocate(m%dx(m%nx))
-       if( m % x(m%nx) < m % x(1) ) then
-          call m%log(FPDE_LOG_INFO, "generate_x: x(:) runs backward")
-       end if
-
-    else
-       call m%log(FPDE_LOG_ERROR, "generate_points: x(:) is not associated")
-    end if
-
-  end subroutine generate_points
-
-  !> Deallocate memory associated with mesh, checks if pointers are
-  !! disassociated
-  !!
-  !! @param m
-  !!
-  !! @return
-  !!
-  subroutine free( m )
-    class(mesh), intent(inout) :: m
-
-    if( associated( m % x ) ) then
-       deallocate( m % x )
-    end if
-
-    if( associated( m % dx ) ) then
-       deallocate( m % dx )
-    end if
-
-  end subroutine free
-
-
-  !> Skeleton function, after overloading should calculate \f$D_k f(x_i)\f$.
-  !!
-  !! @param m
-  !! @param f function do differentiate
-  !! @param k k-th derivative
-  !! @param i i-th mesh point
-  !!
-  !! @return \f$D_k f_j(x_i)\f$
-  !!
-  function diff_point( m, f, k, i ) result(d)
-    class(mesh), intent(inout) :: m
-    integer, intent(in) :: i,k
-    real, intent(in) :: f(:)
-    real :: d
-
-    ! assign some arbitrary value to avoid compilation warnings
-    d = 0.
-
-    call m%log(FPDE_LOG_ERROR, &
-         "ERROR: mesh: diff_point: subroutine not overloaded")
-
-  end function diff_point
-
-
-  !> Skeleton subroutine, if overloaded should calculate
-  !! \f$D_k f(x_i)\f$ for \f$k\f$ given as argument and save it to
-  !! df
-  !!
-  !! @param m
-  !! @param f function do differentiate
-  !! @param df derivative is going to be saved here
-  !! @param k rank of derivative to calculate
-  !!
-  subroutine diff_global( m, f, df, k )
-    class(mesh), target, intent(inout) :: m
-    integer, intent(in) :: k
-    real, intent(in) :: f(:)
-    real, intent(out) :: df(:)
-
-    ! assign some arbitrary value to avoid compilation warnings
-    df=0.
-
-    call m%log(FPDE_LOG_ERROR,&
-         "internal_global_d: called with storage_active = .false.")
-
-  end subroutine diff_global
-
-  !> Calculate integral of f (approximation of
-  !! \f$\int_{x_i}^{x_j}f(x)dx\f$) using method of rectangles, works
-  !! also for a nonuniform mesh. \f$(\Delta x_i)_{i=1,n}\f$ are calculated using
-  !! \ref calculate_spacings.
-  !!
-  !! @param m
-  !! @param f vector representing values of function to be integrated
-  !! given at mesh points
-  !!
-  !! @return \f$\sum_{k=j}^{j}\Delta x_k f_k\f$
-  !!
-  function integrate( m, f, i, j ) result(r)
-    class(mesh) :: m
-    integer, intent(in) :: i,j
-    real :: r
-    real, intent(in) :: f(:)
-    real, pointer :: dx(:), x(:)
-    integer :: nx
-
-    ! temporary pointer for brevity
-    dx => m % dx
-
-    ! use m%calculate_spacings to calculate \Delta x_i
-    call m % calculate_spacings
-    ! calculate sum \Delta x_i f_i
-    r = sum(f(i:j)*dx(i:j))
-
-  end function integrate
-
-  !> calculates \f$(\Delta x_i)_{i=1,n}\f$ using the following formula
-  !! \f$\Delta x_1 = \frac{x_2-x_1}{2}\f$, \f$\Delta x_n =
-  !! \frac{x_n-x_{n-1}}{2}\f$, \f$\Delta x_i =
-  !! \frac{x_{i+1}-x_{i-1}}{2}\f$ for the method of rectangles
-  !!
-  !! @param m
-  !!
-  !! @return
-  !!
-  subroutine calculate_spacings(m)
-    class(mesh) :: m
-    real, pointer :: dx(:), x(:)
-    integer :: nx
-    ! set local pointers and variables for brevity
-    dx => m % dx
-    x  => m % x
-    nx = m % nx
-
-    ! calculate mesh spacing
-    dx(2:nx-1) = (x(3:nx) - x(1:nx-2))/2.
-    dx(1) = (x(2)-x(1))/2.
-    dx(nx) = (x(nx)-x(nx-1))/2.
-  end subroutine calculate_spacings
+  end interface
 
 end module class_mesh
---
+++ src/mesh/mesh/sfd3pt.f90
@@ -0,0 +1,95 @@
+!>
+!! @file   sfd3pt.f90
+!! @author Pawel Biernat <pawel.biernat@gmail.com>
+!! @date   Sat May 12 19:54:09 2012
+!!
+!! @brief An example mesh implementing a symmetric finite difference
+!! method of order 2
+!!
+!!
+!!
+module class_mesh_sfd3pt
+  use constants_module
+  use logger_module
+  use class_mesh
+
+  private
+
+  type, public, extends( mesh ) :: mesh_sfd3pt
+   contains
+     ! overloaded procedures go here (if needed)
+     procedure :: init
+     procedure :: diff_global
+     procedure :: diff_point
+  end type mesh_sfd3pt
+
+contains
+
+  subroutine init( p, error )
+    class(mesh_sfd3pt), intent(inout) :: p
+    integer, optional, intent(out) :: error
+
+    if(present(error)) error = FPDE_STATUS_OK
+
+    p % name = "sfd3pt"
+    p % ghost_points = 1
+    p % max_derivative = 1
+
+  end subroutine init
+
+  subroutine diff_global( m, f, x, df, k )
+    class(mesh_sfd3pt), target, intent(inout) :: m
+    real, intent(in) :: f(:), x(:)
+    real, intent(out) :: df(:)
+    integer, intent(in) :: k
+
+    integer :: j, n
+    real :: h
+
+    if( k > m%max_derivative ) then
+       call m%log(FPDE_LOG_ERROR,&
+            "Too large rank of derivative to calculate with this mesh")
+       return
+    end if
+
+    n = size(f)
+    h = x(2) - x(1)             ! we assume x is a uniform grid
+
+    if( k == 1 ) then
+       forall( j = 2 : n - 1 )
+          df(j)=(f(j+1)-f(j-1))/h/2.
+       end forall
+       df(1) = (f(2)-f(1  ))/h
+       df(n) = (f(n)-f(n-1))/h
+    end if
+
+  end subroutine diff_global
+
+  function diff_point( m, f, x, k, i ) result(d)
+    class(mesh_sfd3pt), target, intent(inout) :: m
+    integer, intent(in) :: i,k
+    real, intent(in) :: f(:), x(:)
+    real :: d
+
+    integer :: j, n
+    real :: h
+
+    if( k > m%max_derivative ) then
+       call m%log(FPDE_LOG_ERROR,&
+            "Too large rank of derivative to calculate with this mesh")
+       return
+    end if
+
+    n = size(f)
+    h = x(2) - x(1)             ! we assume x is a uniform grid
+
+    if( k == 1 ) then
+       if( 1 < i .and. i < n ) d = (f(i+1)-f(i-1))/h/2.
+       if( i == 1 )            d = (f(2  )-f(1  ))/h
+       if( i == n )            d = (f(n  )-f(n-1))/h
+    end if
+
+  end function diff_point
+
+
+end module class_mesh_sfd3pt
