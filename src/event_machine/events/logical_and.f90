9a22c90c1c99c95640a6bfc7d341681ecb1589cd
Paweł Biernat <pawel.biernat@gmail.com>
2013-04-21 20:29:47 +0200

    Added a first implementation of logical operators on events

---
+++ src/event_machine/events/logical_and.f90
@@ -0,0 +1,47 @@
+module class_event_and
+
+  use class_event
+  use class_event_logical_base
+  use class_icicles_user
+  use constants_module
+
+  private
+
+  type, public, extends(event_logical_base) :: event_and
+   contains
+     procedure :: test
+  end type event_and
+
+  interface event_and
+     module procedure :: ev_and
+  end interface event_and
+
+contains
+
+  function ev_and(op1, op2) result(r)
+    class(event), target, intent(in) :: op1, op2
+    type(event_and), pointer :: r
+    allocate(r)
+    r%op1 => op1
+    r%op2 => op2
+  end function ev_and
+
+  function test(ev, ic, error) result(r)
+    class(event_and) :: ev
+    class(icicles_user) :: ic
+    integer, optional, intent(out) :: error
+
+    logical :: r
+    integer :: err1, err2
+
+    if(present(error)) error = FPDE_STATUS_OK
+
+    r = ev%op1%test(ic, err1) .and. ev%op2%test(ic, err2)
+
+    if( err1 /= FPDE_STATUS_OK .or. err2 /= FPDE_STATUS_OK ) then
+       if(present(error)) error = FPDE_STATUS_ERROR
+    end if
+
+  end function test
+
+end module class_event_and
